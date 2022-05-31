4a30ee88644a02edbc216365b38ffb01f542da7a
Sadra Khorami <30285010+SadraKhorami@users.noreply.github.com>
2022-05-31 00:08:12 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+﻿const DiscordJS = require('discord.js')
+const request = require('request');
+const config = require('./config.json');
+setInterval(function () {
+  request({
+    url: `https://www.instagram.com/${config.username}/?__a=1`,
+    method: "GET",
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+      'Referer': "https://www.instagram.com/accounts/login/",
+      'x-csrftoken': "",
+      'Cookie': "rur=; shbid=; csrftoken=; shbts=; sessionid=; ds_user_id=; ig_did=; mid=",
+      'Accept': '/',
+      'Connection': 'keep-alive'
+    },
+    json: true
+  }, function (_error, _response, body) {
+    console.log("Followers updated to " + body["graphql"]["user"]["edge_followed_by"]["count"]);
+    client.channels.cache.get(config.vchannel).setName("🟡 INSTAGRAM: " + body["graphql"]["user"]["edge_followed_by"]["count"])
+  })
+}, 600000);
+
+
+const client = new DiscordJS.Client({
+  partials: ['MESSAGE', 'REACTION'],
+})
+
+client.on('ready', () => {
+  console.log(`${client.user.username} is Ready.`);
+  setInterval(() => {
+    let membersCount = client.guilds.cache.map(guild => guild.memberCount).reduce((a, b) => a + b, 0)
+    client.user.setActivity(`${membersCount} Users 🌹`, { type: "WATCHING" });
+  }, 180000);
+})
+
+
+client.login(config.token)
